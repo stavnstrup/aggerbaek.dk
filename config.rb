cfe1f3a7d3e89fdfcdb8dbecdd8fb1b7b1017a90
Jens Stavnstrup <stavnstrup@gmail.com>
2013-02-10 03:36:14 +0100

    Modify configuration files

---
+++ config.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Compass configuration for the aggerbaek.dk stylesheets
+
+require "zurb-foundation"
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+line_comments = false
+preferred_syntax = :scss
+
+# The output style for the compiled css.
+# One of: :nested, :expanded, :compact, or :compressed. 
+output_style = :expanded
