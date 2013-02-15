316cc633bb07fca66143e4f3c9ef6c88cf1be991
Jens Stavnstrup <stavnstrup@gmail.com>
2013-02-15 23:41:47 +0100

    Replace gallery with an orbit gallery

--- config.rb
+++ config.rb
@@ -5,8 +5,11 @@ require "zurb-foundation"
 http_path = "/"
 sass_dir = "scss"
 css_dir = "css"
+images_dir = "images"
 line_comments = false
 preferred_syntax = :scss
+relative_asset = true
+javascripts_dir = "javascripts"
 
 # The output style for the compiled css.
 # One of: :nested, :expanded, :compact, or :compressed. 
