c276c65b732e153603cbac92c6f4276e60a0f0d9
Jens Stavnstrup <stavnstrup@gmail.com>
2013-02-22 16:24:46 +0100

    Modify the audio/video page for testing purposes

---
+++ includes/_header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>Aggerb&aelig;k - <?= $page_title; ?></title>
+  <meta name="description" content="Aggerbæk 'My Oh My' - New Album out now.">
+  <meta name="keywordsS" content="aggerbæk, jazz, my oh my, when true love says goodbye, you don't know, a spark, we'll die alone, in the dark, be together apart, st. peter's blues, orange moon, lost in love">
+  <meta name="author" content="Christina Olsson, Webdesigner, E Foqus Danmark A/S">
+
+  <!-- Included CSS Files -->
+  <link rel="stylesheet" href="/css/aggerbaek.css">
+
+  <script src="/javascripts/foundation/modernizr.foundation.js"></script>
+</head>
+<body>
+  <div class="row">
+    <div class="twelve columns">
+      <img id="logo" src="/images/aggerbaek-logo.jpg" alt=""/>
+    </div>
+  </div>
+  <div class="row">
+    <div class="twelve columns">
+      <img id="banner" src="/images/aggerbaek.jpg" alt="Aggerb&aelig;k" width="1000" height="251" border="0" href="index.html" />
+    </div>
+  </div>
