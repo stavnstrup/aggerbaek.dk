308ba57846310332d916d6d7a04b4f9564c1193d
Jens Stavnstrup <stavnstrup@gmail.com>
2013-02-23 10:50:55 +0100

    Use PHP header and footer on all pages

---
+++ calendar.php
@@ -0,0 +1,69 @@
+<? $page_title = "Calendar" ?>
+<?php include("includes/_header.php"); ?>
+
+  <div class="row">
+    <div class="twelve columns">
+      <div class="contain-to-grid">
+        <nav class="top-bar" id="menubar">
+          <ul>
+            <li class="toggle-topbar"><a href="#"></a></li>
+          </ul>
+          <section>
+            <ul class="left">
+              <li><a href="index.html" title="News">news</a></li>
+              <li><a href="press.html" title="Press">press</a></li>
+              <li><a href="biography.html" title="">biography</a></li>
+              <li><a href="discography.html" title="">discography</a></li>
+              <li><a href="gallery.html" title="">gallery</a></li>
+              <li><a href="lyrics.html" title="">lyrics</a></li>
+              <li><a href="audiovideo.html" title="">audio/video</a></li>
+              <li class="active"><a href="calendar.html" title="">calendar</a></li>
+              <li><a href="contact.html" title="">contact</a></li>
+            </ul>
+          </section>
+        </nav>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="twelve columns">
+      <h5>Calendar 2012</h5>
+
+
+
+      <div class="month">
+        <h6>January</h6>
+        <ul>
+          <li><span class="day">6</span><span class="time">16:30</span>Aggerbæk @ Buddhas Jazzklub, Klaverfabrikken, Hillerød</li>
+          <li><span class="day">23</span><span class="time">20:00</span>Aggerbæk @ Kind of Blue</li>
+          <li><span class="day">28</span><span class="time">15:00</span>Aggerbæk @ Køge Nytårskur</li>
+        </ul>
+      </div>
+
+      <div class="month">
+        <h6>February</h6>
+        <ul>
+          <li><span class="day">04</span><span class="time">20:30</span>Aggerbæk Kvintet @ Vinterjazz på Islands Brygge Kulturhus (K.I.B)</span></li>
+          <li><span class="day">09</span><span class="time">21:00</span>Benjamin Aggerbæk + Søren Baun + Kasper Bai præsenterer moderne vokaljazz @ Vinterjazz Café Retro</li>
+          <li><span class="day">11</span><span class="time">17:00</span>Benjamin Aggerbæk Pladerelease på "Heartbeat" @ Galathea Kroens Kælderbar</li>
+          <li><span class="day">16</span><span class="time">21:00</span>
+          Benjamin Aggerbæk + Søren Baun + Kasper Bai præsenterer moderne vokaljazz @ Vinterjazz Café Retro</li>
+        </ul>
+      </div> 
+    </div>
+  </div>
+
+  <footer>
+    <div class="row">
+      <div class="twelve columns">
+        <p class="text-right">Website/Design:  Christina Olsson, <a href="http://www.efoqus.dk">E Foqus Danmark A/S</a></p>
+      </div>
+    </div>
+  </footer>
+
+
+<?php include("includes/_footer.php"); ?>
+
+</body>
+</html>
