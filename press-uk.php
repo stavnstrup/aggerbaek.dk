e6c5251fb0a42c2add13e78309cc32b446c7d868
Jens Stavnstrup <stavnstrup@gmail.com>
2013-03-13 08:48:03 +0100

    Minor formatting problems

--- press-uk.php
+++ press-uk.php
@@ -32,8 +32,9 @@
 
       <p><a href="pdf/AggerbaekQuintetPressRelease.pdf">Aggerbaek Quintet press  release in English</a></p>
 
-      <p><strong>Jazz Special - #112 (Dec. 09)</strong><br>
-      Henrik Wolsgaard-Iversen:<br> &ldquo;The concept spans many angles and
+      <p><strong>Jazz Special - #112 (Dec. 09)<br>
+      Henrik Wolsgaard-Iversen:</strong><br>
+      &ldquo;The concept spans many angles and
       colors, from the simple folk/blues to the more voluptuous
       orchestrations with a full horn section and extra strings. But
       it is the singer and his songs - it's Aggerbaek all along - that
@@ -42,26 +43,31 @@
       beautifully... &rdquo;</p>
 
       <p><strong>Nation-wide Danish newspaper Information -
-      22-12-09</strong><br> Peter H. Larsen:<br> &quot;One cannot
+      22-12-09<br> Peter H. Larsen:</strong><br>
+      &quot;One cannot
       listen to &ldquo;My Oh My&rdquo; without having
       fun...[Aggerbaek] is a huge vocal talent. &quot;</p>
 
       <p><strong>Nation-wide Danish newspaper Politiken -
-      03-12-09</strong><br> Henrik Palle:<br> &ldquo;[Aggerbaek] plays
+      03-12-09<br> Henrik Palle:</strong><br>
+      &ldquo;[Aggerbaek] plays
       his own songs, which he performs with great feeling, aided by
       varied and beautiful graces from Kasper Bai, who plays guitar in
       Aggerbaek's ensemble... Aggerbaek's vocals possess a great
       fervor and stands up to the very imaginitive musical background
       that gives the songs such a large and full expression...Charming
-      and absolutely endearing .&rdquo;<br> Regional newspaper Aarhus
-      Stifttidende 10-11-09<br> Thomas Bj&oslash;rnsteen:<br>
+      and absolutely endearing .&rdquo;</p>
+
+      <p><strong>Regional newspaper Aarhus Stifttidende 10-11-09<br>
+      Thomas Bj&oslash;rnsteen:</strong><br>
       &ldquo;Absolutely crucial is Aggerbaek's charming voice that
       both nestles in softly and vibrates intensely ... you believe in
       the little stories about love, oblivion and darkness. Elegant
       and persuasive. &rdquo;</p>
 
       <p><strong>JazzNyt by Niels Overg&aring;rd -
-      28-12-2009</strong><br> Jazznyt.blogspot.com<br> &ldquo;It is
+      28-12-2009<br> Jazznyt.blogspot.com</strong><br>
+      &ldquo;It is
       all too rare that Danish singers make original and new jazz
       music, but it is not only because Aggerbaek is one such rare
       fish that he has made one of the year's best records...the ease
