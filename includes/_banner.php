f766bda87c7a9ddf0ed20d633629470bdc23729b
Jens Stavnstrup <stavnstrup@gmail.com>
2013-11-10 17:15:15 +0100

    Add social icons

--- includes/_banner.php
+++ includes/_banner.php
@@ -1,5 +1,12 @@
     <div class="row">
       <div class="twelve columns">
         <img id="banner" src="/images/aggerbaek.jpg" alt="Aggerb&aelig;k" width="1000" height="251" border="0" href="index.php" />
+        <div class="social">
+          <ul>
+            <li><a href="https://www.facebook.com/pages/Aggerb%C3%A6k/16548534356"><i class="fa fa-facebook-square"></i></a></li>
+            <li><i class="fa fa-youtube-square"></i></li>
+            <li><i class="fa fa-apple"></i><span class="itunes">iTunes</span></li>
+          </ul>
+        </div>
       </div>
     </div>
