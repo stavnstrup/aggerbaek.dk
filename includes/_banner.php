bb9e44aea77107ca28d0309ffd5d42b2cd452848
Jens Stavnstrup <stavnstrup@gmail.com>
2013-11-12 09:01:11 +0100

    Remove obsolete code

--- includes/_banner.php
+++ includes/_banner.php
@@ -3,19 +3,6 @@
         <img id="banner" src="/images/aggerbaek.jpg" alt="Aggerb&aelig;k" width="1000" height="251" border="0" href="index.php" />
       </div>
     </div>
-<!--
-    <div class="row">
-      <div class="twelve columns">
-        <div class="social right">
-          <ul>
-            <li><a href="https://www.facebook.com/pages/Aggerb%C3%A6k/16548534356"><i class="fa fa-facebook-square"></i></a></li>
-            <li><a href="http://www.youtube.com/user/BenjaminAggerbaek"><i class="fa fa-youtube-square"></i></a></li>
-            <li><a href="https://itunes.apple.com/us/artist/aggerb-k-kvintet/id282913483"><i class="fa fa-apple"></i><span class="itunes">iTunes</span></a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
--->
     <div class="row">
       <div class="twelve columns">
         <div class="social right">
