07a146a6c03f140329ab26a10995d6cf84f3fa50
Jens Stavnstrup <stavnstrup@gmail.com>
2013-11-15 13:20:52 +0100

    Use share concert calendar

---
+++ includes/_concerts.php
@@ -0,0 +1,47 @@
+      <h5>Koncerter 2013</h5>
+
+      <div class="month">
+        <h6>November</h6>
+        <ul>
+          <li><span class="day">28</span><span class="time">22:30</span><a href="http://www.drop-inn.dk/">Aggerbæk Orchestra @ Drop-In, København</a></li>
+          <li><span class="day">22</span><span class="time">21:00</span><a href="https://www.facebook.com/events/1381189632101784/?source=1">Reason#1 (Love) Benjamin Aggerbæk @ Operaen, Christiania</a></li>
+        </ul>
+      </div>
+
+
+      <div class="month">
+        <h6>Oktober</h6>
+        <ul>
+          <li><span class="day">10</span><span class="time">19:00</span><a href="http://www.klaverfabrikkenlive.dk/arrangementer/vis/78/aggerbaek-orchestra/">Aggerbæk Orchestra @ Buddhas Jazzklub, Hillerød</a></li>
+        </ul>
+      </div>
+
+      <div class="month">
+        <h6>April</h6>
+        <ul>
+          <li><span class="day">16</span><span class="time">19:00</span>Heartbeat Release Party @ Planeten, Huset i Magstræde</li>
+        </ul>
+      </div>
+<!--
+
+      <div class="month">
+        <h6>February</h6>
+        <ul>
+          <li><span class="day">04</span><span class="time">20:30</span>Aggerbæk Kvintet @ Vinterjazz på Islands Brygge Kulturhus (K.I.B)</span></li>
+          <li><span class="day">09</span><span class="time">21:00</span>Benjamin Aggerbæk + Søren Baun + Kasper Bai præsenterer moderne vokaljazz @ Vinterjazz Café Retro</li>
+          <li><span class="day">11</span><span class="time">17:00</span>Benjamin Aggerbæk Pladerelease på "Heartbeat" @ Galathea Kroens Kælderbar</li>
+          <li><span class="day">16</span><span class="time">21:00</span>
+          Benjamin Aggerbæk + Søren Baun + Kasper Bai præsenterer moderne vokaljazz @ Vinterjazz Café Retro</li>
+        </ul>
+      </div>
+
+      <div class="month">
+        <h6>January</h6>
+        <ul>
+          <li><span class="day">6</span><span class="time">16:30</span>Aggerbæk @ Buddhas Jazzklub, Klaverfabrikken, Hillerød</li>
+          <li><span class="day">23</span><span class="time">20:00</span>Aggerbæk @ Kind of Blue</li>
+          <li><span class="day">28</span><span class="time">15:00</span>Aggerbæk @ Køge Nytårskur</li>
+        </ul>
+      </div>
+
+--> 
