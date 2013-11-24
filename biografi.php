b0e90343bc907d9345ae50898fdd1631720b1830
Jens Stavnstrup <stavnstrup@gmail.com>
2013-11-24 17:44:55 +0100

    Modify biography

--- biografi.php
+++ biografi.php
@@ -48,28 +48,26 @@
           <p>Benjamin Aggerbæk er en københavnerbaseret komponist,
           sanger og ildsjæl.</p>
 
-          <p>Hans nu fire albums dækker alt fra det folk’et til det
-          jazz’et – fra en helt nøgen soloudgivelse med kun Aggerbæks
-          skrøbelige vokal og akustisk guitar til stort anlagte
-          orkesterpladder med masser af blæs og stryg. Benjamin
+          <p>Hans nu 4 albums dækker alt fra det folk’ede til det
+          jazz’ede – fra en helt nøgen soloudgivelse med kun Aggerbæks
+          farverige vokal og akustisk guitar til stort anlagte
+          orkesterplader med masser af blæs og stryg. Benjamin
           Aggerbæk mestrer begge formater og optræder både alene med
           sin guitar i bedste singersongwriter stil og som moderne
-          jazzcrooner i front for hans 15 mands store Aggerbæk
+          jazzcrooner i front for sit 15 mands store Aggerbæk
           Orchestra, der leverer charmerende swing og klassisk
           inderlighed.</p>
 
-           <p>Aggerbæk er således en mand med mange projekter ; man
-           kan høre hans musik på radioen, hvor flere sange fra hans
-           seneste album var blandt de mest spillede på P8, møde ham
-           ugentligt på en af hans sangskriverscener eller til et af
-           DMF-Københavns mange initiativer, hvor Benjamin sidder i
-           bestyrelsen. Benjamin er modtager af såvel DMF-købenahvns
-           ildsjælspris 2012 og den nordsjællandske jazzfestival ”New
-           Note Festival”s ærespris for arbejdet for originalmusikken
-           på de ugentlige open-mic sangskriver-scener i København og
-           omegn samt debutpladen ”Back In Town” i 2008.</p>
-
-
+          <p>Aggerbæk er således en mand med mange projekter; man kan
+          høre hans musik på radioen, hvor flere sange fra hans
+          seneste album er blandt de mest spillede på P8 Jazz, møde
+          ham ugentligt på en af hans sangskriverscener eller til et
+          af DMF-Københavns mange initiativer, hvor Benjamin sidder i
+          bestyrelsen. Benjamin er modtager af såvel DMF-københavns
+          ildsjælspris 2012 og den nordsjællandske jazzfestival ”New
+          Note Festival”s ærespris for arbejdet for originalmusikken
+          på de ugentlige open-mic sangskriver-scener i København og
+          omegn samt debutpladen ”Back In Town” i 2008.</p>
 
         </div>
         <div class="six
